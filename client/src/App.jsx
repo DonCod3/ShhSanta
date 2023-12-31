d296c8e2e068d3f7b630849ded9d64ade511e264
Shamar MELVIN <shamar@MacBook-Pro.local>
2023-12-31 14:22:02 -0500

    tailwinding

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 // import { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import './index.css'
 import Home  from './pages/Home.jsx'
+
 
 
 function App() {
